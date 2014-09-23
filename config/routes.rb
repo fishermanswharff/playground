408f3d77f722d8f3e7c7d5bfa9cffd5e3494a41b
fishermanswharff <fishermanswharff@github.com>
2014-09-23 11:06:10 -0400

    Initializing my playground

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :high_scores
+  root 'greetings#hello'
+  get 'greetings/hello' => 'greetings#hello'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
