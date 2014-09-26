29635ca42d71d29b234368a14e87149cf16bbded
fishermanswharff <fishermanswharff@github.com>
2014-09-26 12:23:07 -0400

    updating views, _form partial, CharacterController now creates, reads, updates, and destroys characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -5,16 +5,41 @@ class CharactersController < ApplicationController
   end
 
   def new
+    @character = Character.new
   end
 
   def create
     @character = Character.new(character_params)
-    @character.save
-    redirect_to @character
+
+    if @character.save
+      redirect_to @character
+    else
+      render 'new'
+    end
   end
 
   def show
     @character = Character.find(params[:id])
+  end
+
+  def edit
+    @character = Character.find(params[:id])
+  end
+
+  def update
+    @character = Character.find(params[:id])
+    if @character.update(character_params)
+      redirect_to @character
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @character = Character.find(params[:id])
+    @character.destroy
+
+    redirect_to characters_path
   end
 
   private
